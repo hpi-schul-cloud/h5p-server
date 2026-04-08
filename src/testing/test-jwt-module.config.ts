285d988e596c68fa61a0b86bed4339b6956cc47d
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-04-08 15:52:37 +0200

    refactor: remove public access modifier from TestJwtModuleConfig properties

--- src/testing/test-jwt-module.config.ts
+++ src/testing/test-jwt-module.config.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { ConfigProperty, Configuration } from '@infra/configuration';
 import { Transform } from 'class-transformer';
 import { IsString } from 'class-validator';
@@ -11,22 +10,22 @@ export class TestJwtModuleConfig {
 	@ConfigProperty('JWT_PRIVATE_KEY')
 	@IsString()
 	@Transform(({ value }: { value: string }) => value.replace(/\\n/g, '\n'))
-	public privateKey!: string;
+	privateKey!: string;
 
 	@ConfigProperty('JWT_PUBLIC_KEY')
 	@IsString()
 	@Transform(({ value }: { value: string }) => value.replace(/\\n/g, '\n'))
-	public publicKey!: string;
+	publicKey!: string;
 
 	@ConfigProperty('JWT_SIGNING_ALGORITHM')
 	@IsString()
-	public algorithm: Algorithm = 'RS256';
+	algorithm: Algorithm = 'RS256';
 
 	@ConfigProperty('JWT_LIFETIME')
 	@IsString()
-	public expiresIn = '30d';
+	expiresIn = '30d';
 
 	@ConfigProperty('SC_DOMAIN')
 	@IsString()
-	public scDomain!: string;
+	scDomain!: string;
 }
