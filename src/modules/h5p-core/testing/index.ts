667bda4c3193e6843c42f2c4f5457184a03e319a
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-05-22 12:33:43 +0200

    Add installed-library.factory for generating test instances of InstalledLibrary

--- src/modules/h5p-core/testing/index.ts
+++ src/modules/h5p-core/testing/index.ts
@@ -2,5 +2,6 @@ export * from './h5p-content-do.factory';
 export * from './h5p-content.factory';
 export * from './h5p-copy-content-params.factory';
 export * from './h5p-entity-library.test.factory';
+export * from './installed-library.factory';
 export * from './library-administration-overview-item.test.factory';
 export * from './library-install-result.test.factory';
