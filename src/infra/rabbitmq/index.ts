1b7be3cac8f6a5731768a7be2589e2b84d57cbc0
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-06-12 14:05:09 +0200

    BC-11713 - Remove ShutdownCallback export from AMQP connection guard

--- src/infra/rabbitmq/index.ts
+++ src/infra/rabbitmq/index.ts
@@ -1,4 +1,4 @@
-export { AmqpConnectionGuard, ShutdownCallback } from './amqp-connection-guard.service';
+export { AmqpConnectionGuard } from './amqp-connection-guard.service';
 export { registerAmqpSubscriber } from './amqp-subscriber.helper';
 export { ErrorMapper } from './error.mapper';
 export { AmqpConnectionLostLoggable } from './loggable';
