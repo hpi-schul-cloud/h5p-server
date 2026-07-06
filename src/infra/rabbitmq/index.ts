70b4192116f80ed60445d3aa7c690651eadd4d81
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-07-06 10:40:55 +0200

    BC-11984 - Clean up unused code using knip

--- src/infra/rabbitmq/index.ts
+++ src/infra/rabbitmq/index.ts
@@ -1,11 +1,6 @@
 export { registerAmqpSubscriber } from './amqp-subscriber.helper';
 export { ErrorMapper } from './error.mapper';
-export {
-	InternalRabbitMQConfig,
-	InternalRabbitMQExchangeConfig,
-	RabbitMQExchangeType,
-	RabbitMQModuleOptions,
-} from './rabbitmq-module.options';
+export { InternalRabbitMQExchangeConfig, RabbitMQExchangeType } from './rabbitmq-module.options';
 export { RABBITMQ_CONFIG_TOKEN, RabbitMQConfig } from './rabbitmq.config';
 export { RabbitMQWrapperModule } from './rabbitmq.module';
 export { RpcError, RpcMessage } from './rpc-message';
