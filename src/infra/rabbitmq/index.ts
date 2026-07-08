33ef625f808048a205cd54acc4d0a69389d6eb05
Constantin Bergatt <constantin.bergatt@dataport.de>
2026-07-08 07:51:41 +0200

    BC-11984 - Revert changes in `src/infra` and add the folder to knip's ignore list

--- src/infra/rabbitmq/index.ts
+++ src/infra/rabbitmq/index.ts
@@ -1,6 +1,11 @@
 export { registerAmqpSubscriber } from './amqp-subscriber.helper';
 export { ErrorMapper } from './error.mapper';
-export { InternalRabbitMQExchangeConfig, RabbitMQExchangeType } from './rabbitmq-module.options';
+export {
+	InternalRabbitMQConfig,
+	InternalRabbitMQExchangeConfig,
+	RabbitMQExchangeType,
+	RabbitMQModuleOptions,
+} from './rabbitmq-module.options';
 export { RABBITMQ_CONFIG_TOKEN, RabbitMQConfig } from './rabbitmq.config';
 export { RabbitMQWrapperModule } from './rabbitmq.module';
 export { RpcError, RpcMessage } from './rpc-message';
