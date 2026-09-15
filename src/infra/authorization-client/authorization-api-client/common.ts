415144f3e52f070408cd9ceaf3e7b5f7781605e7
virgilchiriac <17074330+virgilchiriac@users.noreply.github.com>
2026-09-15 11:12:21 +0200

    SVSDEV-114  - chore: update minor dependency versions (#79)
    - browserslist [high]
    - fast-uri [high]
    - ip-address [high]
    - nanoid [high]

--- src/infra/authorization-client/authorization-api-client/common.ts
+++ src/infra/authorization-client/authorization-api-client/common.ts
@@ -14,7 +14,7 @@
 
 import type { Configuration } from "./configuration";
 import type { RequestArgs } from "./base";
-import type { AxiosInstance, AxiosResponse } from 'axios';
+import type { AxiosInstance, AxiosResponse, AxiosRequestConfig } from 'axios';
 import { RequiredError } from "./base";
 
 /**
@@ -141,9 +141,9 @@ export const toPathString = function (url: URL) {
  *
  * @export
  */
-export const createRequestFunction = function (axiosArgs: RequestArgs, globalAxios: AxiosInstance, BASE_PATH: string, configuration?: Configuration) {
-    return <T = unknown, R = AxiosResponse<T>>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
-        const axiosRequestArgs = {...axiosArgs.options, url: (axios.defaults.baseURL ? '' : configuration?.basePath ?? basePath) + axiosArgs.url};
-        return axios.request<T, R>(axiosRequestArgs);
+export const createRequestFunction: (axiosArgs: RequestArgs, globalAxios: AxiosInstance, BASE_PATH: string, configuration?: Configuration) => <T = unknown>(axios?: AxiosInstance, basePath?: string) => Promise<AxiosResponse<T>> = function (axiosArgs: RequestArgs, globalAxios: AxiosInstance, BASE_PATH: string, configuration?: Configuration) {
+    return <T = unknown>(axios: AxiosInstance = globalAxios, basePath: string = BASE_PATH) => {
+        const axiosRequestArgs: AxiosRequestConfig = {...axiosArgs.options, url: (axios.defaults.baseURL ? '' : configuration?.basePath ?? basePath) + axiosArgs.url};
+        return axios.request<T, AxiosResponse<T>>(axiosRequestArgs);
     };
 }
